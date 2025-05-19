a9f79bc24f1031256d9daa8ad0be779a902d14c0
ting436 <tinghui1804@gmail.com>
2025-05-19 21:55:14 +0800

    add tabs for long term and short term todos

--- functions.py
+++ functions.py
@@ -10,3 +10,21 @@ def get_todos(filepath=FILEPATH):
 def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
+def get_weekly_goals(filepath="weekly_goals.txt"):
+    with open(filepath, 'r') as file_local:
+        weekly_goals_local = file_local.readlines()
+    return weekly_goals_local
+
+def write_weekly_goals(weekly_goals_arg, filepath="weekly_goals.txt"):
+    with open(filepath, 'w') as file:
+        file.writelines(weekly_goals_arg)
+
+def get_overall_goals(filepath="overall_goals.txt"):
+    with open(filepath, 'r') as file_local:
+        overall_goals_local = file_local.readlines()
+    return overall_goals_local
+
+def write_overall_goals(overall_goals_arg, filepath="overall_goals.txt"):
+    with open(filepath, 'w') as file:
+        file.writelines(overall_goals_arg)
--- web.py
+++ web.py
@@ -2,27 +2,72 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
+weekly_goals = functions.get_weekly_goals()
+overall_goals = functions.get_overall_goals()
 
-
+# Function to add a new todo
 def add_todo():
     new_todo = st.session_state["new_todo"] + "\n"
     todos.append(new_todo)
     functions.write_todos(todos)
 
+# Function to add a new weekly goal
+def add_weekly_goal():
+    new_goal = st.session_state["new_weekly_goal"] + "\n"
+    weekly_goals.append(new_goal)
+    functions.write_weekly_goals(weekly_goals)
 
-st.title("My Todo App")
-st.subheader("This is my todo app")
-st.write("This is to increase your productivity.")
+# Function to add a new overall goal
+def add_overall_goal():
+    new_goal = st.session_state["new_overall_goal"] + "\n"
+    overall_goals.append(new_goal)
+    functions.write_overall_goals(overall_goals)
 
+# Main app
+st.title("My Productivity App")
+st.subheader("Organize your tasks and goals effectively!")
 
-for index, todo in enumerate(todos):
-    checked = st.checkbox(todo, key=todo)
-    if checked:
-        todos.pop(index)
-        functions.write_todos(todos)
-        del st.session_state[todo]
-        st.rerun()
+# Tabs for different sections
+tabs = st.tabs(["Todos", "Weekly Goals", "Overall Goals"])
 
-st.text_input(label="", placeholder="Add a new todo...",
-              on_change=add_todo, key='new_todo')
+# Todos Tab
+with tabs[0]:
+    st.header("Todos")
+    st.write("Manage your daily tasks here.")
+    for index, todo in enumerate(todos):
+        checked = st.checkbox(todo, key=todo)
+        if checked:
+            todos.pop(index)
+            functions.write_todos(todos)
+            del st.session_state[todo]
+            st.rerun()
+    st.text_input(label="Add a new todo", placeholder="Add a new todo...",
+                  on_change=add_todo, key='new_todo')
 
+# Weekly Goals Tab
+with tabs[1]:
+    st.header("Weekly Goals")
+    st.write("Set and track your weekly goals.")
+    for index, goal in enumerate(weekly_goals):
+        checked = st.checkbox(goal.strip(), key=f"weekly_goal_{index}")
+        if checked:
+            weekly_goals.pop(index)
+            functions.write_weekly_goals(weekly_goals)
+            del st.session_state[f"weekly_goal_{index}"]
+            st.rerun()
+    st.text_input(label="Add a new weekly goal", placeholder="Add a new weekly goal...",
+                  on_change=add_weekly_goal, key='new_weekly_goal')
+
+# Overall Goals Tab
+with tabs[2]:
+    st.header("Overall Goals")
+    st.write("Define your long-term goals.")
+    for index, goal in enumerate(overall_goals):
+        checked = st.checkbox(goal.strip(), key=f"overall_goal_{index}")
+        if checked:
+            overall_goals.pop(index)
+            functions.write_overall_goals(overall_goals)
+            del st.session_state[f"overall_goal_{index}"]
+            st.rerun()
+    st.text_input(label="Add a new overall goal", placeholder="Add a new overall goal...",
+                  on_change=add_overall_goal, key='new_overall_goal')
